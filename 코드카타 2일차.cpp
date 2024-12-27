e9243aa28e32fce8c60c6468d2e543efaa01639e
Hyeok10 <131437196+Hyeok10@users.noreply.github.com>
2024-12-27 09:25:23 +0900

    Add files via upload

---
+++ 코드카타 2일차.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    return num1 * num2;
+}
+
+int main() {
+    cout << solution(3, 4) << endl; // 12
+    cout << solution(27, 19) << endl; // 513
+    return 0;
+}
